f51cf1550ff691e1aeaf0347a3edcd51564346db
Xavier <xmancarey31@gmail.com>
2024-12-05 14:13:55 +1100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# This is a website for my photography
+Yeah. Lol.
+
+It's written in Typescript with React, Vite and Tailwind CSS.
+
+This is my first actual frontend project okay be nice.
+
+<small>
+please
+</small>
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <img src="https://s3.amazonaws.com/static.graphemica.com/glyphs/i500s/000/010/229/original/0058-500x500.png?1275328238" alt="logo" className="w-24 h-24 mb-4" />
+        <div className="text-2xl text-rose-400">xuniversal</div>
+      </div>
+    </>
+  )
+}
+
+export default App
