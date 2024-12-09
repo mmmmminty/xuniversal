d950f5c914fd922920186b2ea039e19058470ae5
Xavier Carey <xmancarey31@gmail.com>
2024-12-10 04:38:21 +1100

    ready for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Routes } from "react-router-dom"
 import ComingSoon from "./components/main/ComingSoon"
 import Navbar from "./components/main/Navbar"
 
